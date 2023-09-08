f96c2c4dafd78552a4a5fe56c946bb5d34f4aa45
Bojan Bakaric <bakaricbojan@gmail.com>
2023-09-08 10:05:12 +0000

    Wire up debug to os, migrate and create superuser

--- cvek_art_gallery/settings.py
+++ cvek_art_gallery/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY_CAG', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-boiann-cvekartgallery-hmompk0hxtl.ws-eu104.gitpod.io', 'localhost']
 
