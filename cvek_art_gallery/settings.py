32d10d176472c3ddf2384f04790870dc9e354a29
Bojan Bakaric <114668562+Boiann@users.noreply.github.com>
2023-09-09 11:28:16 +0100

    Base templates (#81)
    * Add allauth and base.html template
    
    * Add blocks to base template
    
    * Add home app and templates

--- cvek_art_gallery/settings.py
+++ cvek_art_gallery/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -82,7 +83,10 @@ ROOT_URLCONF = 'cvek_art_gallery.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
