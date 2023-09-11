15521c4d95fb5310f8e544fc8a71d444ce179c43
Bojan Bakaric <bakaricbojan@gmail.com>
2023-09-11 14:25:50 +0000

    Wire up and add paintings view and template

--- paintings/views.py
+++ paintings/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Painting
 
-# Create your views here.
+
+def all_paintings(request):
+    """ A view to show all paintings, including sorting and search queries """
+
+    paintings = Painting.objects.all()
+
+    context = {
+        'paintings': paintings,
+    }
+
+    return render(request, 'paintings/paintings.html', context)
