640ecde78e3204758a395109bead52a5b70628e5
Bojan Bakaric <bakaricbojan@gmail.com>
2023-09-12 10:35:33 +0000

    Add search functionality

--- paintings/views.py
+++ paintings/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Painting
 
 
@@ -6,9 +8,21 @@ def all_paintings(request):
     """ A view to show all paintings, including sorting and search queries """
 
     paintings = Painting.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('paintings'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            paintings = paintings.filter(queries)
 
     context = {
         'paintings': paintings,
+        'search_term': query,
     }
 
     return render(request, 'paintings/paintings.html', context)
