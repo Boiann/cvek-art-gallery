bfc2731116761e50eba079c664289ebfdec55756
Bojan Bakaric <bakaricbojan@gmail.com>
2023-09-18 14:01:10 +0000

    Fix checkout models, migrate, add checkout sucess logic

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
+from decimal import Decimal
 from .forms import OrderForm
+from .models import Order, OrderLineItem
+from paintings.models import Painting
 from cart.contexts import cart_contents
 
 import stripe
@@ -12,21 +15,80 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    cart = request.session.get('cart', {})
-    if not cart:
-        messages.error(request, "There's nothing in your cart at the moment")
-        return redirect(reverse('paintings'))
+    if request.method == 'POST':
+        cart = request.session.get('cart', [])
 
-    current_cart = cart_contents(request)
-    total = current_cart['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save()
+            for item_data in cart:
+                try:
+                    painting = Painting.objects.get(id=item_data['id'])
+                    frame = item_data.get('frame', None)
+                    frame_price = Decimal('0.00')
 
-    order_form = OrderForm()
+                    if frame == 'standard_frame':
+                        frame_price = Decimal('50.00')
+                    elif frame == 'premium_frame':
+                        frame_price = Decimal('100.00')
+
+                    base_price = painting.price
+
+                    if painting.subcategory.filter(name='clearance').exists():
+                        clearance_discount = Decimal('0.20')
+                        discounted_price = base_price - (base_price * clearance_discount)
+                    else:
+                        discounted_price = base_price
+
+                    total_price = discounted_price + frame_price
+
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        painting=painting,  # Associate the order line item with the actual Painting object
+                        frame=frame,
+                        # Other fields...
+                    )
+                    order_line_item.save()
+                except Painting.DoesNotExist:
+                    messages.error(request, (
+                        "One of the products in your bag wasn't found in our database. "
+                        "Please call us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_cart'))
+
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
+    else:
+        cart = request.session.get('cart', [])
+        if not cart:
+            messages.error(request, "There's nothing in your bag at the moment")
+            return redirect(reverse('paintings'))
+
+        current_cart = cart_contents(request)
+        total = current_cart['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
@@ -37,6 +99,27 @@ def checkout(request):
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
+    }
+
+    return render(request, template, context)
+
+
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+
+    if 'cart' in request.session:
+        del request.session['cart']
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
     }
 
     return render(request, template, context)
