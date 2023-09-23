71c8923e929cf9591b25e46053efe48d8e6db1a4
Bojan Bakaric <bakaricbojan@gmail.com>
2023-09-23 09:19:43 +0000

    Refactor painting form to correctly display cat & subcategories

--- paintings/forms.py
+++ paintings/forms.py
@@ -2,6 +2,10 @@ from django import forms
 from .models import Painting, Category, SubCategory
 
 
+from django import forms
+from .models import Painting, Category, SubCategory
+
+
 class PaintingForm(forms.ModelForm):
 
     class Meta:
@@ -11,13 +15,11 @@ class PaintingForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
-        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+        friendly_names_categories = [(c.id, c.get_friendly_name()) for c in categories]
         subcategories = SubCategory.objects.all()
-        friendly_names = [(s.id, s.get_friendly_name()) for s in subcategories]
+        friendly_names_subcategories = [(s.id, s.get_friendly_name()) for s in subcategories]
 
-        self.fields['category'].choices = friendly_names
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black rounded-0'
-        self.fields['subcategory'].choices = friendly_names
+        self.fields['category'].choices = friendly_names_categories
+        self.fields['subcategory'].choices = friendly_names_subcategories
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
