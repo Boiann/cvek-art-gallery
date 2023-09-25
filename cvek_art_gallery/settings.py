49731054f7529bc798d3365fcf58056873bfc7e2
Bojan Bakaric <bakaricbojan@gmail.com>
2023-09-25 18:30:30 +0000

    Fix html image paths

--- cvek_art_gallery/settings.py
+++ cvek_art_gallery/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY_CAG', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-boiann-cvekartgallery-hmompk0hxtl.ws-eu104.gitpod.io', 'localhost',
